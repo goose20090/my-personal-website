97510169fa6adb3c498ff702df35de4d7dcab347
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-09 18:15:04 +0100

    installed slideshow

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 let nrc= document.getElementById("nrc");
 
 nrc.addEventListener("mouseenter", function(event){
@@ -7,14 +34,3 @@ nrc.addEventListener("mouseenter", function(event){
 nrc.addEventListener("mouseleave", function(event){
     event.target.src = "without-arrow.jpg";
 })
-/*
-let selfie= document.getElementById("selfie")
-
-selfie.addEventListener('mouseover', function(event){
-    event.target.src = '42993.jpeg';
-})
-
-selfie.addEventListener('mouseleave', function(event){
-    event.target.src ='photo-of-me.jpeg';
-   
-})*/
